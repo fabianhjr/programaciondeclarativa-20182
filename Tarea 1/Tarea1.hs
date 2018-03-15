058f81fe9436a5246f5b3283a132c44765ca1f59
Fabián Heredia Montiel <fabianherediajr@gmail.com>
2018-03-15 00:17:56 -0600

    Avance

--- Tarea 1/Tarea1.hs
+++ Tarea 1/Tarea1.hs
@@ -4,10 +4,12 @@ Profesor: C. Moisés Vázquez Reyes
 Ayudante: Enrique Antonio Bernal Cedillo
 -}
 
+module Tarea1 where
+
 import Numeric.Natural(Natural)
-import Data.Maybe(fromMaybe)
 import Data.List(intersect)
 import Unificacion
+
 
 {-----------------------}
 -- CÁLCULO LAMBDA PURO --
@@ -21,21 +23,36 @@ instance Show Lam_U where
              LamU x e   -> "(λ" ++ x ++ "." ++ show e ++ ")"
              AppU e1 e2 -> "(" ++ show e1 ++ " " ++ show e2 ++ ")"
 
+
 --
 -- Variables Libres y Ligadas
 --
+
+-- | Checa si una variable aparece en una expresión
+--
+-- >>> "x" `esVarEn` (LamU "x" (VarU "y"))
+-- False
+-- >>> "x" `esVarEn` (VarU "x")
+-- True
 esVarEn :: Nombre -> Lam_U -> Bool
 esVarEn b e = case e of
               (VarU v)     -> b == v
-              (LamU v e1)  -> b `esVarEn` e1
+              (LamU _ e1)  -> b `esVarEn` e1
               (AppU e1 e2) -> b `esVarEn` e1 || b `esVarEn` e2
 
+-- | Lista todas las variables libres en una expresión
+--
+-- >>> varsLibres (LamU "x" (VarU "x"))
+-- []
+-- >>> varsLibres (LamU "x" (VarU "y"))
+-- ["y"]
 varsLibres :: Lam_U -> [Nombre]
 varsLibres e = case e of
                  (VarU v)     -> [v]
-                 (LamU v e1)  -> varsLibres e1 `sin` v
+                 (LamU v e1)  -> varsLibres e1 `without` v
                  (AppU e1 e2) -> varsLibres e1 ++ varsLibres e2
-  where l `sin` x = filter (/= x) l
+  where l `without` x = filter (/= x) l
+
 
 varsLigadasEn :: Lam_U -> Nombre -> [Nombre]
 varsLigadasEn e b = case e of
@@ -45,8 +62,8 @@ varsLigadasEn e b = case e of
                                       else     varsLigadasEn e1 b
                       (AppU e1 e2) -> varsLigadasEn e1 b ++ varsLigadasEn e2 b
 
-colisionanAlSustituir :: Nombre -> Lam_U -> Lam_U -> Bool
-colisionanAlSustituir b e s = not . null $ varsLigadasEn e b `intersect` varsLibres s
+colisionanAlSustituir :: Nombre -> Lam_U -> Lam_U -> [Nombre]
+colisionanAlSustituir b e s = varsLigadasEn e b `intersect` varsLibres s
 
 --
 -- Sustitución Inocente
@@ -63,7 +80,7 @@ sustituir b e s = case e of
 --
 alphaEquiv :: Lam_U -> Nombre -> Nombre -> Lam_U
 alphaEquiv e b s
-  | colisionanAlSustituir b e (VarU s) = error "Error: Colision"
+  | not . null $ colisionanAlSustituir b e (VarU s) = error "Error: Colision"
   | otherwise = case e of
                   (VarU v)     -> VarU v
                   (LamU v e1)  -> if v == b
@@ -74,8 +91,9 @@ alphaEquiv e b s
 alphaEquivAuto :: Lam_U -> Nombre -> Lam_U
 alphaEquivAuto e b = alphaEquiv e b remplazo
   where
-    posiblesRemplazos = iterate (++ "'") b
-    remplazo = head . dropWhile (\s' -> colisionanAlSustituir b e (VarU s')) $ posiblesRemplazos
+    posiblesRemplazos = tail $ iterate (++ "'") b
+    pruebaRemplazo s' = null $ colisionanAlSustituir b e (VarU s')
+    remplazo = head . filter pruebaRemplazo $ posiblesRemplazos
 
 --
 -- Checa si se puede β-reducir
@@ -88,52 +106,52 @@ betaRed e = case e of
               (AppU e1 e2)        -> betaRed e1 || betaRed e2
 
 --
--- Aplica la primer β-reducción posible.
+-- Aplica una β-reducción si es posible.
 --
 betaR :: Lam_U -> Lam_U
 betaR (AppU (LamU v e) s)
-  | not $ colisionanAlSustituir v e s = sustituir v e s
+  | null colisiones = sustituir v e s
   | otherwise = betaR (AppU (LamU v e') s)
   where
-    primerCol = head $ varsLigadasEn e v `intersect` varsLibres s
-    posiblesRemplazos = iterate (++ "'") $ primerCol
-    remplazo = head . dropWhile (\s' -> colisionanAlSustituir primerCol e (VarU s')) $ posiblesRemplazos
-    v' = if v == primerCol then remplazo else v
-    e' = alphaEquiv e primerCol remplazo
-betaR e = case e of
-            (VarU v)     -> VarU v
-            (LamU v e1)  -> LamU v $ betaR e1
-            (AppU e1 e2) -> if betaRed e1 then AppU (betaR e1) e2
-                            else AppU e1 (betaR e2)
+    colisiones = colisionanAlSustituir v e s
+    e'         = foldl alphaEquivAuto e colisiones
+betaR _ = undefined
 
 --
 -- Busca la formaNormal en orden normal
 --
 formaNormal :: Lam_U -> Lam_U
-formaNormal (VarU x)   = VarU x
-formaNormal (LamU x e) = LamU x (formaNormal e)
+formaNormal (VarU x)    = VarU x
+formaNormal (LamU x e1) = LamU x (formaNormal e1)
 formaNormal (AppU (LamU n c) e2)      = formaNormal $ betaR $ AppU (LamU n c) e2
-formaNormal (AppU e1 e2) | betaRed e1 = formaNormal $ AppU (betaR e1) e2
+formaNormal (AppU e1 e2) | betaRed e1 = formaNormal $ AppU (formaNormal e1) e2
                          | betaRed e2 = AppU e1 (formaNormal e2)
                          | otherwise  = AppU e1 e2
 
 --
 -- Definiciones Generales en λ
 --
-true'  = LamU "x" $ LamU "y" $ VarU "x"
-false' = LamU "x" $ LamU "y" $ VarU "y"
-not'   = LamU "p" $ LamU "x" $ LamU "y" $ AppU (AppU (VarU "p") (VarU "y")) (VarU "x")
-pair   = LamU "x" $ LamU "y" $ LamU "p" $ AppU (AppU (VarU "p") (VarU "x")) (VarU "y")
-fst'   = LamU "p" $ AppU (VarU "p") true'
-snd'   = LamU "p" $ AppU (VarU "p") false'
+lTrue  :: Lam_U
+lTrue  = LamU "x" $ LamU "y" $ VarU "x"
+lFalse :: Lam_U
+lFalse = LamU "x" $ LamU "y" $ VarU "y"
+lNot   :: Lam_U
+lNot   = LamU "p" $ LamU "x" $ LamU "y" $ AppU (AppU (VarU "p") (VarU "y")) (VarU "x")
+lPair  :: Lam_U
+lPair  = LamU "x" $ LamU "y" $ LamU "p" $ AppU (AppU (VarU "p") (VarU "x")) (VarU "y")
+lFst   :: Lam_U
+lFst   = LamU "p" $ AppU (VarU "p") lTrue
+lSnd   :: Lam_U
+lSnd   = LamU "p" $ AppU (VarU "p") lFalse
 
 --
 -- Numerales de Church
 --
-succ' = LamU "n" $ LamU "s" $ LamU "z" $ AppU (VarU "s") $ AppU (AppU (VarU "n") (VarU "s")) (VarU "z")
+lSucc :: Lam_U
+lSucc = LamU "n" $ LamU "s" $ LamU "z" $ AppU (VarU "s") $ AppU (AppU (VarU "n") (VarU "s")) (VarU "z")
 churchN :: Natural -> Lam_U
 churchN 0 = LamU "s" $ LamU "z" $ VarU "z"
-churchN n = formaNormal $ AppU succ' (churchN (n-1))
+churchN n = formaNormal $ AppU lSucc (churchN (n-1))
 
 --
 -- Numerales de Scott
@@ -146,54 +164,77 @@ scottN n = LamU "x" $ LamU "y" $ AppU (VarU "y") (scottN (n-1))
 --
 --Codifica los incisos de la pregunta 1
 --
+f1 :: Lam_U
 f1 = LamU "n" $ LamU "m" $ LamU "s" $ LamU "z" $ AppU (AppU (AppU (VarU "m") (VarU "n")) (VarU "s")) (VarU "z")
+g1 :: Lam_U
 g1 = LamU "n" $ LamU "s" $ LamU "z" $ AppU (AppU (AppU (VarU "n") g1') (LamU "u" (VarU "z"))) (LamU "u" (VarU "u"))
   where g1' = LamU "h1" $ LamU "h2" $ AppU (VarU "h2") (AppU (VarU "h1") (VarU "s"))
-h1 = LamU "n" $ AppU fst' (AppU (AppU (VarU "n") ss) zz)
-  where ss = LamU "p" (AppU (AppU pair (AppU snd' (VarU "p"))) (AppU succ' (AppU snd' (VarU "p"))))
-        zz = AppU (AppU pair (churchN 0)) (churchN 0)
+h1 :: Lam_U
+h1 = LamU "n" $ AppU lFst (AppU (AppU (VarU "n") ss) zz) -- λn.fst (n ss zz)
+  where ss = LamU "p" $ AppU (AppU lPair (AppU lSnd (VarU "p"))) (AppU lSucc (AppU lSnd (VarU "p"))) -- λp.pair (snd p) (suc (snd p)),
+        zz = AppU (AppU lPair (churchN 0)) (churchN 0) -- pair 0 0
 
 --Cálculos
 -- f1 5 0 = λs.λz.s z = 1
+res1 :: Lam_U
 res1 = formaNormal (AppU (AppU f1 (churchN 5)) (churchN 0))
 -- f1 2 3 = λs.λz.s(s(s(s(s(s(s(s z))))))) = 8
+res2 :: Lam_U
 res2 = formaNormal (AppU (AppU f1 (churchN 2)) (churchN 3))
 
 -- g1 0 = λs.λz.z = 0
+res3 :: Lam_U
 res3 = formaNormal (AppU g1 (churchN 0))
 -- g1 3 = λs.λz.s(s z)) = 2
+res4 :: Lam_U
 res4 = formaNormal (AppU g1 (churchN 3))
 
 -- h1 1 = λs.λz.z = 0
+res5 :: Lam_U
 res5 = formaNormal (AppU h1 (churchN 1))
 -- h1 2 = λs.λz'.s z' = 1
+res6 :: Lam_U
 res6 = formaNormal (AppU h1 (churchN 2))
 
 --Codifica los incisos de la pregunta 2
-f2 = LamU "n" $ LamU "x" $ LamU "y" $ AppU (VarU "y") (VarU "n")
-g2 = LamU "n" $ AppU (AppU (VarU "n") (scottN 0)) (LamU "x" (VarU "x"))
-h2 = LamU "n" $ AppU (AppU (VarU "n") true') (LamU "x" false')-- λn.n true (λx.false)
+f2 :: Lam_U
+f2 = LamU "n" $ LamU "x" $ LamU "y" $ AppU (VarU "y") (VarU "n") -- λn.λx.λy.yn
+-- TODO: Verificar porque no es pred.
+g2 :: Lam_U
+g2 = LamU "n" $ AppU (AppU (VarU "n") (scottN 0)) (LamU "x" (VarU "x")) -- λn.n 0 (λx.x)
+h2 :: Lam_U
+h2 = LamU "n" $ AppU (AppU (VarU "n") lTrue) (LamU "x" lFalse) -- λn.n true (λx.false)
 
 -- Cálculos
 -- f2 0 = λx.λy.y(λx.λy.x) = 1
+res7 :: Lam_U
 res7 = formaNormal (AppU f2 (scottN 0))
 -- f2 3 = λx.λy.y(λx.λy.y(λx.λy.y(λx.λy.y(λx.λy.x)))) = 4
+res8 :: Lam_U
 res8 = formaNormal (AppU f2 (scottN 3))
 
 -- g2 1 = λx.λy.x = 0
+res9 :: Lam_U
 res9 = formaNormal (AppU g2 (scottN 1))
 -- g2 4 = λx.λy.y(λx.λy.y(λx.λy.y(λx.λy.x))) = 3
+res10 :: Lam_U
 res10 = formaNormal (AppU g2 (scottN 4))
 
 -- h2 0 = λx.λy.x = True
+res11 :: Lam_U
 res11 = formaNormal (AppU h2 (scottN 0))
 -- h2 5 = λx.λy.y = False
+res12 :: Lam_U
 res12 = formaNormal (AppU h2 (scottN 5))
 
 --Codifica los incisos de la pregunta 3
+yCombinator :: Lam_U
 yCombinator = LamU "f" $ AppU (LamU "x" (AppU (VarU "f") (AppU (VarU "x") (VarU "x")))) (LamU "x" (AppU (VarU "f") (AppU (VarU "x") (VarU "x"))))
 
-casiImpar = LamU "f" $ LamU "n" $ AppU (AppU (AppU h2 (VarU "n")) false') (AppU not' (AppU (VarU "f") (AppU g2 (VarU "n"))))
+-- TODO: Pendiente a resolver g2
+casiImpar :: Lam_U
+casiImpar = LamU "f" $ LamU "n" $ AppU (AppU (AppU h2 (VarU "n")) lFalse) (AppU lNot (AppU (VarU "f") (AppU g2 (VarU "n"))))
+impar :: Lam_U
 impar = formaNormal (AppU yCombinator casiImpar)
 
 {-----------------------}
